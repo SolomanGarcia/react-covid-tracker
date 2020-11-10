d55784e059072fa64feb8660177ecf5c3ea7a025
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-10 15:54:34 -0700

    work on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>Covid Tracker</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
