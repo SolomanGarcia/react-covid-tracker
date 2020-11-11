98bda2771e441eb7f68352814254214037f74509
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-11 09:58:37 -0700

    create Table component

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import {
 } from "@material-ui/core";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -33,6 +35,7 @@ function App() {
             value: country.countryInfo.iso2
           }));
 
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -98,7 +101,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-
+          <Table countries={tableData} />
           <h3>World Wide New Cases</h3>
         </CardContent>
       </Card>
