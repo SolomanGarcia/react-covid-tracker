06e9cffb4cd77f527f978e106890fa74a779258e
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-11 11:22:10 -0700

    install Numeral, work on chart

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Table from "./Table";
+import LineGraph from "./LineGraph";
 import "./App.css";
 import { sortData } from "./util";
 
