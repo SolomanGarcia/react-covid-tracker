905c53ec7a9041dea066fdd3182781d1eaf101eb
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-12 13:20:18 -0700

    debug line graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -46,32 +46,32 @@ const options = {
   }
 };
 
+const buildChartData = (data, casesType = "cases") => {
+  const chartData = [];
+  let lastDataPoint;
+
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
 function LineGraph({ casesType = "cases", ...props }) {
   const [data, setData] = useState({});
-
-  const buildChartData = (data, casesType = "cases") => {
-    const chartData = [];
-    let lastDataPoint;
-
-    for (let date in data.cases) {
-      if (lastDataPoint) {
-        const newDataPoint = {
-          x: date,
-          y: data[casesType][date] - lastDataPoint
-        };
-        chartData.push(newDataPoint);
-      }
-      lastDataPoint = data[casesType][date];
-    }
-    return chartData;
-  };
 
   useEffect(() => {
     const fetchData = async () => {
       await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
         .then((response) => response.json())
         .then((data) => {
-          const chartData = buildChartData(data);
+          let chartData = buildChartData(data, casesType);
           setData(chartData);
         });
     };
